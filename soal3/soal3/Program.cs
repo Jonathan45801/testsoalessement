3ee44fcdcaa66aaf4b792f3a2e3c0eb0f4616528
jonathan <43164516+Jonathan45801@users.noreply.github.com>
2023-09-21 19:19:57 +0700

    soal 3

---
+++ soal3/soal3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace soal3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(perkaliansederhana(5, 4));
+
+        }
+        public static int perkaliansederhana(int j, int k)
+        {
+            int hasil = 0;
+            while (j > 0)
+            {
+                hasil += k;
+                j--;
+            }
+            return hasil;
+        }
+    }
+}
